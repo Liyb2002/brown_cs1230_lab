7ad80333e46ad86e75c063a02a331e2852d31802
Liyb2002 <yl4092@columbia.edu>
2022-08-01 02:28:56 -0400

    invert

--- lab09/filter/FilterInvert.cpp
+++ lab09/filter/FilterInvert.cpp
@@ -7,22 +7,27 @@ FilterInvert::~FilterInvert()
 
 void FilterInvert::apply(Canvas2D *canvas) {
 
-    /*
+
     int width = canvas->width();
 
     RGBA* current_pixel = nullptr;
     RGBA* current_row = canvas->data();
 
+    RGBA* data = canvas->data();
+    size_t currentIndex = 0;
+
+
     // TODO: Task 8 [Optional] Use bitwise complement to set inversion
 
     for (int r = 0; r < canvas->height(); r++) {
         current_pixel = current_row;
+        currentIndex = r * width;
 
         for (int c = 0; c < canvas->width(); c++) {
             // TODO: Task 7 Update the color with its inversion
-            current_pixel->r = 255 - current_pixel->r;
-            current_pixel->g = 255 - current_pixel->g;
-            current_pixel->b = 255 - current_pixel->b;
+            data[currentIndex+c].r = 255 -data[currentIndex+c].r;
+            data[currentIndex+c].g = 255 - data[currentIndex+c].g;
+            data[currentIndex+c].b = 255 - data[currentIndex+c].b;
 
 
             // CONTINUED: Task 8 [Optional]
@@ -30,5 +35,5 @@ void FilterInvert::apply(Canvas2D *canvas) {
             current_pixel++;
         }
         current_row += width;
-    }*/
+    }
 }
